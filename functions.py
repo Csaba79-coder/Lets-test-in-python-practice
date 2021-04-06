c565fb844e8373fb4f508ade7fecd2c857d12bc8
Csaba79-coder <csabavadasz79@gmail.com>
2021-04-06 18:23:26 +0200

    Summary
    make it visible

---
+++ functions.py
@@ -0,0 +1,14 @@
+def add_two_integers(a, b):
+    pass
+    return (a + b)
+
+
+def is_integer_a_greater_than_integer_b(a, b):
+    pass
+    return (a > b)
+
+
+def say_hello(name):
+    pass
+    return ("Hello, " + name +"!")
+
